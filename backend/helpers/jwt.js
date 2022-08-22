7a94ffc5981f70f0e82fc99f7d6841f3b7611fdc
Olga <olganum@gmail.com>
2022-08-23 01:08:24 +0300

    правка readme

--- backend/helpers/jwt.js
+++ backend/helpers/jwt.js
@@ -1,14 +1,14 @@
 const jwt = require('jsonwebtoken'); // импортируем модуль jsonwebtoken
 
-const { NODE_ENV, JWT_SECRET } = process.env;
+const { JWT_SECRET = 'dev-secret'} = process.env;
 
 // payload - это то, что хотим зашифровать
 // eslint-disable-next-line arrow-body-style
 const generateToken = (payload) => {
-  return jwt.sign(payload, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' }); // что хотим зашифровать и ключ
+  return jwt.sign(payload, JWT_SECRET, { expiresIn: '7d' }); // что хотим зашифровать и ключ
 };
 
-const checkToken = (token) => jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+const checkToken = (token) => jwt.verify(token, JWT_SECRET);
 
 module.exports = {
   generateToken,
