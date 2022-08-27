9e0357a7227c77f296d0851c6f14e531e09f6f7b
Olga <olganum@gmail.com>
2022-08-28 00:50:58 +0300

    39

--- backend/app.js
+++ backend/app.js
@@ -14,8 +14,8 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { validateUser, validateAuthorization } = require('./middlewares/validation');
 
 // Слушаем 3000 порт
-const { PORT = 3001 } = process.env;
-// const { PORT = 3000 } = process.env;
+// const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 
 const app = express();
 // app.use(cors());
